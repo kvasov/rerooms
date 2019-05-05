081a31c83b0825dac85302ce62b2f71010f8ae88
Dmitry Kvasov <kvasov85@mail.ru>
2019-05-05 21:49:12 +0300

    reviews

--- src/js/main.js
+++ src/js/main.js
@@ -22,4 +22,16 @@ $(document).ready(() => {
         });
     });
   }
+
+  $('.js-reviews-more').click(function () {
+    $('.reviews__list .reviews-item:hidden:lt(2)').css({
+      display: 'block'
+    });
+
+    if (
+      $('.reviews__list .reviews-item').length == $('.reviews__list .reviews-item:visible').length
+    ) {
+      $(this).remove();
+    }
+  });
 });
