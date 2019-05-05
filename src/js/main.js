4790b44a25dfb62a838c8748f73dcbed31b994c5
Dmitry Kvasov <kvasov85@mail.ru>
2019-05-05 15:23:57 +0300

    supervision, form

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,25 @@
 import 'babel-polyfill';
+
+$(document).ready(() => {
+  const $inputItem = $('.js-inputWrapper');
+  if ($inputItem.length) {
+    $inputItem.each(function () {
+      let $this = $(this),
+        $input = $this.find('.form-row__input'),
+        placeholderTxt = $input.attr('placeholder'),
+        $placeholder;
+
+      $input.after(`<span class="placeholder">${placeholderTxt}</span>`),
+      $input.attr('placeholder', ''),
+      ($placeholder = $this.find('.placeholder')),
+      $input.val().length ? $this.addClass('active') : $this.removeClass('active'),
+      $input
+        .on('focusout', () => {
+          $input.val().length ? $this.addClass('active') : $this.removeClass('active');
+        })
+        .on('focus', () => {
+          $this.addClass('active');
+        });
+    });
+  }
+});
